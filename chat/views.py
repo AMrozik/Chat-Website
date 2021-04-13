8f3f82812e7da1939aa98f363c2165843c3be22e
bartoszpotrykus <bartoszpotrykus@icloud.com>
2021-04-13 17:48:02 +0200

    Add consumer to receive and send messages

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
 
-def index(response):
-    return HttpResponse("There will be chat someday :p")
+def index(request):
+    return render(request, 'chat/index.html')
+
+
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name': room_name
+    })
